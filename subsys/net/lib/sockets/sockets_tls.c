7acc202c94074399808a5b68605709dd5be4aa35
Pete Skeggs <peter.skeggs@nordicsemi.no>
2022-02-02 13:57:36 -0800

    net: sockets: improve mbed TLS debug readability
    The mbed TLS log messages already include line
    termination, so passing that to NET_DBG() results
    in extra whitespace between every mbed TLS log line.
    
    Remove the final character within the
    already-duplicated string.
    
    Signed-off-by: Pete Skeggs <peter.skeggs@nordicsemi.no>

--- subsys/net/lib/sockets/sockets_tls.c
+++ subsys/net/lib/sockets/sockets_tls.c
@@ -229,8 +229,11 @@ static void tls_debug(void *ctx, int level, const char *file,
 		}
 	}
 
-	NET_DBG("%s:%04d: |%d| %s", basename, line, level,
-		log_strdup(str));
+	char *log = log_strdup(str);
+
+	/* Remove extra EOL to improve log readability. */
+	log[strlen(str) - 1] = '\0';
+	NET_DBG("%s:%04d: |%d| %s", basename, line, level, log);
 }
 #endif /* defined(MBEDTLS_DEBUG_C) && (CONFIG_NET_SOCKETS_LOG_LEVEL >= LOG_LEVEL_DBG) */
 
